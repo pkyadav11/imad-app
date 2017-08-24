a5087bcf649a343db97c051bfa3d0583403a390b
Pankaj Yadav <Pankaj Yadav>
2017-08-25 01:21:05 +0530

    made some changes locally edited server.js

--- server.js
+++ server.js
@@ -13,22 +13,35 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/firstPage',function(req,res){
-    res.send('First page requested and will be served');
-    }
-);
+  res.sendFile(path.join(__dirname, 'ui', 'firstPage.html'));
+  });
+
 app.get('/secondPage',function(req,res){
-    res.send('Second page requested and will be served');
-    }
-);
+  res.sendFile(path.join(__dirname, 'ui', 'secondPage.html'));
+  });
 /*app.get('/firstPage',function(req,res){
     res.send('First page requested and will be served');
     }
 ); */
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.jpg'));
 });
-
+app.get('/ui/em.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'em.jpg'));
+});
+app.get('/ui/circle.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'circle.jpg'));
+});
+app.get('/ui/hp.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'hp.jpg'));
+});
+app.get('/ui/perks.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'perks.jpg'));
+});
+app.get('/ui/noah.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'noah.jpg'));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
